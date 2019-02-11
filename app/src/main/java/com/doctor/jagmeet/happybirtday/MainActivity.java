f11f2ba39a293ca8929cc6da13a71e8b31a2a92d
sjagmeet37 <sjagmeet37@gmail.com>
2019-02-11 20:45:42 +0530

    Initial commit

---
+++ app/src/main/java/com/doctor/jagmeet/happybirtday/MainActivity.java
@@ -0,0 +1,44 @@
+package com.doctor.jagmeet.happybirtday;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    //TextView bday;
+    MediaPlayer mediaPlayer;
+    int TIME_OUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        overridePendingTransition( android.R.anim.fade_out ,android.R.anim.slide_in_left);
+
+        //    bday = findViewById(R.id.mtext);
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.bdaysong);
+        mediaPlayer.start();
+        mediaPlayer.isLooping();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(i);
+                finish();
+            }
+        }, TIME_OUT);
+
+    }
+
+
+
+
+}
